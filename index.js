ea9fe35f11da8bad4430ab8ae6b30c44d0b60d43
demian <ericdemian.martinez@gmail.com>
2021-06-15 17:11:47 -0500

    ADDED new dependencies

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 ;const server = require ('./src/server');
 const dbConnect = require ('./src/lib/db');
 
@@ -5,11 +6,10 @@ const PORT = process.env.PORT || 8080;
 
 dbConnect ()
  .then (() => { 
-     console.log ('DB Connected')
-     server.listen (8080, () => {
+     console.log ('DB Connected');
+     server.listen (PORT, () => {
         console.log('Server is listening')
     })
-
  })
  .catch (error => {
      console.error ('DB connection error: ', error)
